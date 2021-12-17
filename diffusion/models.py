5726b147bb417b95852619c216dcf315df681b2e
Christoph.Heindl <cheind@profactor.at>
2021-12-17 12:00:37 +0100

    refactored to support more layers if needed

--- diffusion/models.py
+++ diffusion/models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-
+from torch.nn.parameter import Parameter
 from . import losses
 
 
@@ -12,18 +12,22 @@ class ToyScoreModel(pl.LightningModule):
         loss: losses.ScoreMatchingLoss,
         n_input: int = 2,
         n_hidden: int = 64,
+        n_hidden_layers: int = 2,
     ):
         super().__init__()
-        self.w1 = torch.nn.Linear(n_input, n_hidden)
-        self.w2 = torch.nn.Linear(n_hidden, n_hidden)
-        self.w3 = torch.nn.Linear(n_hidden, n_input)
+        layers = [torch.nn.Linear(n_input, n_hidden)]
+        layers += [
+            torch.nn.Linear(n_hidden, n_hidden) for _ in range(n_hidden_layers - 1)
+        ]
+        self.head = torch.nn.Linear(n_hidden, n_input)
+        self.layers = torch.nn.ModuleList(layers)
         self.loss_fn = loss
         self.save_hyperparameters()
 
     def forward(self, x):
-        x = F.softplus(self.w1(x))
-        x = F.softplus(self.w2(x))
-        return self.w3(x)
+        for lay in self.layers:
+            x = F.softplus(lay(x))
+        return self.head(x)
 
     def training_step(self, batch, batch_idx):
         del batch_idx
