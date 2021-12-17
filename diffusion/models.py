b983f2bf76d74ada838ba1d3d4dde450001f6749
Christoph Heindl <christoph.heindl@gmail.com>
2021-12-17 19:00:19 +0100

    refactored toy-model implementation

--- diffusion/models.py
+++ diffusion/models.py
@@ -1,8 +1,9 @@
+from typing import Type
 import torch
 import torch.nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from torch.nn.parameter import Parameter
+
 from . import losses
 
 
@@ -15,19 +16,19 @@ class ToyScoreModel(pl.LightningModule):
         n_hidden_layers: int = 2,
     ):
         super().__init__()
-        layers = [torch.nn.Linear(n_input, n_hidden)]
+        layers = [self._layer(n_input, n_hidden, bn=False, dp=0.01, act=torch.nn.ELU)]
         layers += [
-            torch.nn.Linear(n_hidden, n_hidden) for _ in range(n_hidden_layers - 1)
+            self._layer(n_hidden, n_hidden, bn=False, dp=0.01, act=torch.nn.ELU)
+            for _ in range(n_hidden_layers - 1)
         ]
-        self.head = torch.nn.Linear(n_hidden, n_input)
-        self.layers = torch.nn.ModuleList(layers)
+        layers += [self._layer(n_hidden, n_input)]
+        self.layers = torch.nn.Sequential(*layers)
+        print(layers)
         self.loss_fn = loss
         self.save_hyperparameters()
 
     def forward(self, x):
-        for lay in self.layers:
-            x = F.softplus(lay(x))
-        return self.head(x)
+        return self.layers(x)
 
     def training_step(self, batch, batch_idx):
         del batch_idx
@@ -37,3 +38,20 @@ class ToyScoreModel(pl.LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
+
+    def _layer(
+        self,
+        n_in: int,
+        n_out: int,
+        bn: bool = False,
+        dp: float = 0.0,
+        act: Type[torch.nn.Module] = None,
+    ) -> torch.nn.Sequential:
+        layers = [torch.nn.Linear(n_in, n_out)]
+        if act is not None:
+            layers.append(act())
+        if bn:
+            layers.append(torch.nn.BatchNorm1d(n_out))
+        if dp > 0:
+            layers.append(torch.nn.Dropout(p=dp))
+        return torch.nn.Sequential(*layers)
