7ff1aa00dc48b9d057586375acd2e1b767d2e987
Christoph.Heindl <cheind@profactor.at>
2021-12-17 09:02:07 +0100

    added numerical integration

--- diffusion/examples/toy_matching.py
+++ diffusion/examples/toy_matching.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import torch
@@ -8,7 +10,7 @@ import torch.utils.data
 from torch.distributions.distribution import Distribution
 from torch.utils.data.dataloader import DataLoader
 
-from .. import losses, models
+from .. import losses, models, types
 
 
 def create_gt_distribution():
@@ -51,99 +53,197 @@ def load(path):
     return models.ToyScoreModel.load_from_checkpoint(path)
 
 
+def scores_rect2d(
+    score_model: types.DataScoreModel,
+    xlim: tuple[int, int],
+    ylim: tuple[int, int],
+    n_x: int,
+    n_y: int,
+    device: torch.device = None,
+) -> torch.Tensor:
+    X = torch.linspace(xlim[0], xlim[1], n_x, device=device)
+    Y = torch.linspace(ylim[0], ylim[1], n_y, device=device)
+    U, V = torch.meshgrid(Y, X)
+    UV = torch.stack((V, U), -1)
+    scores = score_model(UV.view(-1, 2)).view(n_y, n_x, 2)
+    return scores  # NxMx2
+
+
+@torch.no_grad()
+def integrate_scores_rect2d(
+    scores: Union[torch.Tensor, types.DataScoreModel],
+    xlim: tuple[int, int],
+    ylim: tuple[int, int],
+    n_x: int,
+    n_y: int,
+    c: float = 0.0,
+    device: torch.device = None,
+) -> torch.tensor:
+
+    if not torch.is_tensor(scores):
+        scores = scores_rect2d(scores, xlim, ylim, n_x, n_y, device)
+
+    # uses direct gradient approximation. hope that u is conservative field.
+
+    u = scores.new_zeros((n_y, n_x))
+    hx = (xlim[1] - xlim[0]) / (n_x - 1)
+    hy = (ylim[1] - ylim[0]) / (n_y - 1)
+
+    tx = 0.5 * (scores[:, 1:, 0] + scores[:, :-1, 0]) * hx
+    ty = 0.5 * (scores[1:, :, 1] + scores[:-1, :, 1]) * hy
+
+    # seed
+    u[0, 0] = c
+    for ix in range(n_x):
+        if ix > 0:
+            u[0, ix] = tx[0, ix - 1] + u[0, ix - 1]
+        for iy in range(1, n_y):
+            u[iy, ix] = ty[iy - 1, ix] + u[iy - 1, ix]
+    return u
+
+
 def main():
     pi = create_gt_distribution()
-    model = train(pi)
+    # model = train(pi)
     model = load("tmp/score_model.ckpt")
     model = model.cuda().eval()
 
-    fig, axs = plt.subplots(1, 2)
+    # fig, axs = plt.subplots(1, 2)
     N = 20
     X = torch.linspace(-3, 3, N)
     Y = torch.linspace(-3, 3, N)
     U, V = torch.meshgrid(X, Y)
     UV = torch.stack((V, U), -1)
     x = UV.view(-1, 2).requires_grad_()
-    S_gt = torch.autograd.grad(pi.log_prob(x).sum(), x)[0]
-    S_gt = S_gt.view(N, N, 2)
-    print(S_gt[-1, -1])
-    axs[0].quiver(
-        X,
-        Y,
-        S_gt[..., 0],
-        S_gt[..., 1],
-        angles="xy",
-        scale_units="xy",
-    )
-    axs[0].set_aspect("equal", adjustable="box")
-    axs[0].set_xlim([-3, 3])
-    axs[0].set_ylim([-3, 3])
+    # S_gt = torch.autograd.grad(pi.log_prob(x).sum(), x)[0]
+    # S_gt = S_gt.view(N, N, 2)
+    # print(S_gt[-1, -1])
+    # axs[0].quiver(
+    #     X,
+    #     Y,
+    #     S_gt[..., 0],
+    #     S_gt[..., 1],
+    #     angles="xy",
+    #     scale_units="xy",
+    # )
+    # axs[0].set_aspect("equal", adjustable="box")
+    # axs[0].set_xlim([-3, 3])
+    # axs[0].set_ylim([-3, 3])
 
-    samples = pi.sample((5000,)).cuda()
-    samplesnp = samples.detach().cpu().numpy()
-    axs[1].hist2d(
-        samplesnp[:, 0],
-        samplesnp[:, 1],
-        cmap="viridis",
-        rasterized=False,
-        bins=128,
-        alpha=0.8,
-    )
-    with torch.no_grad():
-        model = model.cuda().eval()
-        S_pred = model(UV.view(-1, 2).cuda()).view(N, N, 2).cpu()
-    print(S_pred[-1, -1])
-    axs[1].quiver(
-        X,
-        Y,
-        S_pred[..., 0],
-        S_pred[..., 1],
-        color=(1, 1, 1, 1),
-        angles="xy",
-        scale_units="xy",
-    )
+    # samples = pi.sample((5000,)).cuda()
+    # samplesnp = samples.detach().cpu().numpy()
+    # axs[1].hist2d(
+    #     samplesnp[:, 0],
+    #     samplesnp[:, 1],
+    #     cmap="viridis",
+    #     rasterized=False,
+    #     bins=128,
+    #     alpha=0.8,
+    # )
+    # with torch.no_grad():
+    #     model = model.cuda().eval()
+    #     S_pred = model(UV.view(-1, 2).cuda()).view(N, N, 2).cpu()
+    # print(S_pred[-1, -1])
+    # axs[1].quiver(
+    #     X,
+    #     Y,
+    #     S_pred[..., 0],
+    #     S_pred[..., 1],
+    #     color=(1, 1, 1, 1),
+    #     angles="xy",
+    #     scale_units="xy",
+    # )
 
-    axs[1].set_aspect("equal", adjustable="box")
-    axs[1].set_xlim([-3, 3])
-    axs[1].set_ylim([-3, 3])
+    # axs[1].set_aspect("equal", adjustable="box")
+    # axs[1].set_xlim([-3, 3])
+    # axs[1].set_ylim([-3, 3])
 
-    plt.show()
+    # plt.show()
 
     # ------------------------------------------------------
 
-    fig, axs = plt.subplots(1, 2)
-    from ..langevin import ula
+    # fig, axs = plt.subplots(1, 2)
+    # from ..langevin import ula
 
-    x0 = torch.rand(5000, 2) * 6 - 3.0
-    n_steps = 20000
-    with torch.no_grad():
-        samples = ula(model, x0.cuda(), n_steps=n_steps, tau=1e-2, n_burnin=n_steps - 1)
-    samplesnp = samples[-1].detach().cpu().numpy()
-    axs[1].hist2d(
-        samplesnp[:, 0],
-        samplesnp[:, 1],
-        cmap="viridis",
-        rasterized=False,
-        bins=128,
-        alpha=0.8,
-    )
-    axs[1].set_aspect("equal", adjustable="box")
-    axs[1].set_xlim([-3, 3])
-    axs[1].set_ylim([-3, 3])
+    # x0 = torch.rand(5000, 2) * 6 - 3.0
+    # n_steps = 20000
+    # with torch.no_grad():
+    #     samples = ula(model, x0.cuda(), n_steps=n_steps, tau=1e-2, n_burnin=n_steps - 1)
+    # samplesnp = samples[-1].detach().cpu().numpy()
+    # axs[1].hist2d(
+    #     samplesnp[:, 0],
+    #     samplesnp[:, 1],
+    #     cmap="viridis",
+    #     rasterized=False,
+    #     bins=128,
+    #     alpha=0.8,
+    # )
+    # axs[1].set_aspect("equal", adjustable="box")
+    # axs[1].set_xlim([-3, 3])
+    # axs[1].set_ylim([-3, 3])
 
-    samples = pi.sample((5000,))
-    samplesnp = samples.cpu().numpy()
-    axs[0].hist2d(
-        samplesnp[:, 0],
-        samplesnp[:, 1],
-        cmap="viridis",
-        rasterized=False,
-        bins=128,
-        alpha=0.8,
+    # samples = pi.sample((5000,))
+    # samplesnp = samples.cpu().numpy()
+    # axs[0].hist2d(
+    #     samplesnp[:, 0],
+    #     samplesnp[:, 1],
+    #     cmap="viridis",
+    #     rasterized=False,
+    #     bins=128,
+    #     alpha=0.8,
+    # )
+    # axs[0].set_aspect("equal", adjustable="box")
+    # axs[0].set_xlim([-3, 3])
+    # axs[0].set_ylim([-3, 3])
+    # plt.show()
+
+    # -----------------------------------------------------
+    fig, axs = plt.subplots(1, 3)
+    u = integrate_scores_rect2d(
+        model,
+        (-3, 3),
+        (-3, 3),
+        100,
+        100,
+        pi.log_prob(torch.tensor([-3, -3])),
+        torch.device("cuda"),
     )
-    axs[0].set_aspect("equal", adjustable="box")
-    axs[0].set_xlim([-3, 3])
-    axs[0].set_ylim([-3, 3])
+    axs[2].imshow(u.cpu(), extent=(-3, 3, -3, 3), origin="lower")
+
+    N = 100
+    M = 100
+    X = torch.linspace(-3, 3, N)
+    Y = torch.linspace(-3, 3, M)
+    U, V = torch.meshgrid(Y, X)
+
+    UV = torch.stack((V, U), -1)
+    print(UV.shape)  # is MxNx2
+    print(UV[0, 1])  # coordinate order is [x,y]
+    print(UV[1, 0])
+
+    x = UV.view(-1, 2)
+    u_gt = pi.log_prob(x).view(M, N)
+    axs[0].imshow(u_gt, extent=(-3, 3, -3, 3), origin="lower")
+
+    X = torch.linspace(-3, 3, N)
+    Y = torch.linspace(-3, 3, N)
+    U, V = torch.meshgrid(X, Y)
+    UV = torch.stack((V, U), -1)
+    x = x.clone().requires_grad_()
+    scores_gt = torch.autograd.grad(pi.log_prob(x).sum(), x)[0]
+    scores_gt = scores_gt.view(N, N, 2)
+
+    u_gt_int = integrate_scores_rect2d(
+        scores_gt,
+        (-3, 3),
+        (-3, 3),
+        100,
+        100,
+        pi.log_prob(torch.tensor([-3, -3])),
+        scores_gt.device,
+    )
+    axs[1].imshow(u_gt_int, extent=(-3, 3, -3, 3), origin="lower")
+
     plt.show()
 
 
