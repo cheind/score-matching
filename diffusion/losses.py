8fafec801d56ed996e9dc042144a6a46df4c01a5
Christoph.Heindl <cheind@profactor.at>
2021-12-17 12:00:24 +0100

    added denoising score matching loss

--- diffusion/losses.py
+++ diffusion/losses.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn
+import torch.nn.functional as F
 
 from .types import DataScoreModel
 from . import jacobians
@@ -28,6 +29,20 @@ def ism_loss_fast(score_model: DataScoreModel, x: torch.Tensor) -> torch.Tensor:
     return _ism(tr, score_model(x))
 
 
+def dsm_loss(
+    score_model: DataScoreModel, x: torch.Tensor, sigma: float
+) -> torch.Tensor:
+    """Denoising score matching loss.
+    q(y|x) is assumed to be N(y;x,sigma^2) and hence \nabla_y log q = 1/sigma^2 * (x-y)
+
+    The smaller sigma, the larger the gradients since q peaks more rapidly.
+    """
+    xn = x + torch.randn_like(x) * sigma
+    targets = (1 / sigma ** 2) * (x - xn)  # \nabla_y log q(y|x)
+    scores = score_model(xn)
+    return 0.5 * ((scores - targets) ** 2).sum(dim=-1).mean(dim=0)
+
+
 class ScoreMatchingLoss(torch.nn.Module):
     def forward(self, score_model: DataScoreModel, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
@@ -43,3 +58,12 @@ class ISMLoss(ScoreMatchingLoss):
 
     def forward(self, score_model: DataScoreModel, x: torch.Tensor) -> torch.Tensor:
         return self.fn(score_model, x)
+
+
+class DSMLoss(ScoreMatchingLoss):
+    def __init__(self, sigma: float = 1e-2) -> None:
+        super().__init__()
+        self.sigma = sigma
+
+    def forward(self, score_model: DataScoreModel, x: torch.Tensor) -> torch.Tensor:
+        return dsm_loss(score_model, x, sigma=self.sigma)
